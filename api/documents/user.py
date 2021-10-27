8caa3575a362a0f384b2229f9301a520a4be812c
Rey Chen <rachel.s.chen@protonmail.com>
2021-10-27 13:54:56 -0700

    this isn't needed; was just for notes

--- api/documents/user.py
+++ api/documents/user.py
@@ -26,25 +26,3 @@ class User(Document):
     )
     passcode = StringField(required=True, min_length=8, max_length=32)
     is_admin = BooleanField(required=True, default=False)
-
-"""
-
-    pymongo>=3.4
-    dateutil>=2.1.0
-    Pillow>=2.0.0
-    blinker>=1.3
-    aniso8601==8.0.0; python_version < '3.5'
-    aniso8601>=0.82; python_version >= '3.5'
-    jsonschema
-    Flask>=0.8, !=2.0.0
-    werkzeug !=2.0.0
-    pytz
-    six>=1.3.0
-    enum34; python_version < '3.4'
-
-    Werkzeug
-    Jinja
-    MarkupSafe
-    ItsDangerous
-    Click
-"""
