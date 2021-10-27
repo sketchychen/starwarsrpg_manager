756e9342f78f17445f5f7df60ca424dbc9736243
Rey Chen <rachel.s.chen@protonmail.com>
2021-10-27 16:57:21 -0700

    fixed that flask run import error holy shit

--- server/resources.py
+++ server/resources.py
@@ -1,30 +1,22 @@
 from flask_restx import Namespace, Resource, fields
 
-from dao import UserDAO
+from .dao import UserDAO
 
 
 ns = Namespace('api')
 
-new_user = ns.model(
-    'NewUser',
-    {
-        'name': fields.String(required=True, min_length=2, max_length=32),
-        'email': fields.String(required=True, min_length=4, max_length=64),
-        'passcode': fields.String(required=True, min_length=8, max_length=64),
-    }
-)
-
-new_character = ns.model(
-    'NewCharacter',
-    {
-        'name': fields.String(required=True, min_length=2),
-        'species': fields.String(required=True, choices=()),
-        'specialization': fields.String(required=True, min_length=3),
-    }
-)
 
 class Register(Resource):
     dao = UserDAO()
+
+    new_user = ns.model(
+        'NewUser',
+        {
+            'name': fields.String(required=True, min_length=2, max_length=32),
+            'email': fields.String(required=True, min_length=4, max_length=64),
+            'passcode': fields.String(required=True, min_length=8, max_length=64),
+        }
+    )
 
     @ns.expect(new_user, validate=True)
     def post(self):
@@ -51,12 +43,20 @@ class CreateCharacter(Resource):
     """
     TODO: Use Permissions and CharacterDAO
     """
+    new_character = ns.model(
+        'NewCharacter',
+        {
+            'name': fields.String(required=True, min_length=2),
+            'species': fields.String(required=True, choices=()),
+            'specialization': fields.String(required=True, min_length=3),
+        }
+    )
     @ns.expect(new_character, validate=True)
     def post(self):
-        req_json = request.get_json()
+        # req_json = request.get_json()
 
-        _name = req_json.get('name')
-        _species = req_json.get('species')
-        _specialization = req_json.get('specialization')
+        # _name = req_json.get('name')
+        # _species = req_json.get('species')
+        # _specialization = req_json.get('specialization')
 
         pass  # WIP
--- server/run.py
+++ server/run.py
@@ -2,12 +2,12 @@ import os
 import json
 from flask import Flask
 from flask_cors import CORS
-
+from flask_mongoengine import MongoEngine
 from flask_restx import Api
 
+
 from .config import load_config
-from .documents import db
-from .routes import api
+from . import resources
 
 
 env_config = load_config()
@@ -15,28 +15,17 @@ env_config = load_config()
 app = Flask(__name__)
 app.config.from_object(env_config)
 
-
+db = MongoEngine()
 db.init_app(app)
+
+api = Api(version='1.0', title='Star Wars FFG Manager API')
+api.add_namespace(resources.ns)
+resources.ns.add_resource(resources.Register, '/register')
+resources.ns.add_resource(resources.RouteUser, '/user')
+resources.ns.add_resource(resources.CreateCharacter, '/character/create')
 api.init_app(app)
+
 CORS(app)
-
-
-# @app.after_request
-# def after_request(response):
-#     """
-#        Sends back a custom error with {"success", "msg"} format
-#     """
-
-#     if int(response.status_code) >= 400:
-#         response_data = json.loads(response.get_data())
-#         if 'errors' in response_data:
-#             response_data = {
-#                 'success': False,
-#                 'msg': list(response_data["errors"].items())[0][1]
-#             }
-#             response.set_data(json.dumps(response_data))
-#         response.headers.add('Content-Type', 'application/json')
-#     return response
 
 
 @app.shell_context_processor
